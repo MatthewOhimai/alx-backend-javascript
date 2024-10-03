6ae495bdc4aaa28955ba190a5bc5c144faaa242a
OhimaiMatthew <matthew4ohimai15@gmail.com>
2024-10-03 15:07:56 +0100

    Updated

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,15 @@
+/**
+ * Return a promise that resolves or rejects based on the success parameter.
+ *
+ * @param {Boolean} success - Whether the API call is successful.
+ * @returns {Promise} A promise that resolves with a success object or rejects with an error object.
+ */
 export default function getFullResponseFromAPI(success) {
-	return new Promise((resolve, reject) => {
-		if (success) {
-			resolve({ status: 200, body: 'Success' });
-		} else {
-			reject(new Error('The fake API is not working currently'));
-		}
-	});
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
