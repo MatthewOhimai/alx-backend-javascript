e98fb15d41e25231756f43e649e85307139cf4c6
OhimaiMatthew <matthew4ohimai15@gmail.com>
2024-10-03 15:06:00 +0100

    Updated

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,6 +1,14 @@
+/**
+ * Divide the numerator by the denominator.
+ *
+ * @param {Number} numerator - The numerator.
+ * @param {Number} denominator - The denominator.
+ * @throws {Error} If the denominator is 0.
+ * @returns {Number} The result of the division.
+ */
 export default function divideFunction(numerator, denominator) {
-	if(denominator === 0) {
-		throw new Error('cannot divide by 0');
-	}
-	return numerator / denominator;
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  return numerator / denominator;
 }
