9144a0e3d57f6c236f389c113d47941a5215c848
OhimaiMatthew <matthew4ohimai15@gmail.com>
2024-10-03 21:29:22 +0100

    Updated

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,33 +1,39 @@
-// 4-pricing.js
+// Import Currency class
 import Currency from './3-currency.js';
 
 export default class Pricing {
-  constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
-  }
+	constructor(amount, currency) {
+		this._amount = amount;
+		this._currency = currency;
+	}
 
-  get amount() {
-    return this._amount;
-  }
+	// Getter for amount
+	get amount() {
+		return this._amount;
+	}
 
-  set amount(value) {
-    this._amount = value;
-  }
+	// Setter for amount
+	set amount(value) {
+		this._amount = value;
+	}
 
-  get currency() {
-    return this._currency;
-  }
+	// Getter for currency
+	get currency() {
+		return this._currency;
+	}
 
-  set currency(value) {
-    this._currency = value;
-  }
+	// Setter for currency
+	set currency(value) {
+		this._currency = value;
+	}
 
-  displayFullPrice() {
-    return `${this._amount} ${this._currency.displayFullCurrency()}`;
-  }
+	// Method to display full price
+	displayFullPrice() {
+		return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+	}
 
-  static convertPrice(amount, conversionRate) {
-    return amount * conversionRate;
-  }
+	// Static method to convert price
+	static convertPrice(amount, conversionRate) {
+		return amount * conversionRate;
+	}
 }
