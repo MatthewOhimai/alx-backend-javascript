ae524010b0023e901be644a8d8573cb9c2f5bc41
OhimaiMatthew <matthew4ohimai15@gmail.com>
2024-10-03 15:04:34 +0100

    Updated

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,11 +1,18 @@
+/**
+ * Create a guardrail for a math function.
+ *
+ * @param {Function} mathFunction - The math function to guard.
+ * @returns {Array} An array containing the result of the math function and a success message.
+ */
 export default function guardrail(mathFunction) {
-	const queue = [];
-	try {
-		const result = mathFunction();
-		queue.push(result);
-	} catch (error) {
-		queue.push(`Error: ${error.message}`);
-	}
-	queue.push('Guardrail was processed');
-	return queue;
+  const queue = [];
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
 }
