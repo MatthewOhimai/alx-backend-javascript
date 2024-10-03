0fb63bf32d10f836efbe73d12955e8c843f85e21
OhimaiMatthew <matthew4ohimai15@gmail.com>
2024-10-03 15:01:09 +0100

    Updated

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,8 +1,8 @@
 export default function guardrail(mathFunction) {
 	const queue = [];
 	try {
-		const value = mathFunction();
-		queue.push(value);
+		const result = mathFunction();
+		queue.push(result);
 	} catch (error) {
 		queue.push(`Error: ${error.message}`);
 	}
