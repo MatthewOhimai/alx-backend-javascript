585789f6f41130ceca94b584d40a23e0f132d1f0
OhimaiMatthew <matthew4ohimai15@gmail.com>
2024-10-03 21:26:21 +0100

    Updated

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,11 @@
-// 1-make_classrooms.js
-export function initializeRooms() {
-  return [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34),
-  ];
+// Import ClassRoom from 0-classroom.js
+import ClassRoom from './0-classroom.js';
+
+// Function to create an array of ClassRoom objects
+export default function initializeRooms() {
+	return [
+		new ClassRoom(19),
+		new ClassRoom(20),
+		new ClassRoom(34)
+	];
 }
